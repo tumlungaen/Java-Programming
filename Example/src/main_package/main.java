f7a853346b378b7d0b2193398007d9149c3c7b4b
tumlungaen <mr.kittinan@gmail.com>
2022-03-06 15:53:53 +0700

    add test.java to test final exam.

--- Example/src/main_package/main.java
+++ Example/src/main_package/main.java
@@ -3,5 +3,6 @@ package main_package;
 public class main {
 	public static void main( String args[] ) {
 		System.out.println( "Hello World in Java." );
+		test a = new test() ;
 	}//end function
 }//end class
---
+++ Example/src/main_package/test.java
@@ -0,0 +1,7 @@
+package main_package;
+
+public class test {
+    test() {
+        System.out.println( "This is test (CLASS)." ) ;
+    }
+}
