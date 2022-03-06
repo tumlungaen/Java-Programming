0228f73b6c61f28f234035b9e88893d105bf872c
tumlungaen <mr.kittinan@gmail.com>
2022-03-06 16:01:14 +0700

    fix test.java

--- Example/src/main_package/main.java
+++ Example/src/main_package/main.java
@@ -2,7 +2,7 @@ package main_package;
 
 public class main {
 	public static void main( String args[] ) {
-		System.out.println( "Hello World in Java." );
 		test a = new test() ;
 	}//end function
 }//end class
+
--- Example/src/main_package/test.java
+++ Example/src/main_package/test.java
@@ -3,5 +3,5 @@ package main_package;
 public class test {
     test() {
         System.out.println( "This is test (CLASS)." ) ;
-    }
-}
+    }//end method
+}//end class
