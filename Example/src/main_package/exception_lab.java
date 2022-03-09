e8ee4b801ea903abc77e6698810e7e2c25a5cb3b
tumlungaen <mr.kittinan@gmail.com>
2022-03-09 11:58:26 +0700

    add exception in lab

---
+++ Example/src/main_package/exception_lab.java
@@ -0,0 +1,10 @@
+package main_package;
+
+public class exception_lab {
+    public static void main( String args[] ) {
+        int[] MyNum = { 1, 2, 3, 4, 5 } ;
+        for( int i = 0 ; i <= 5 ; i++ ) {
+            System.out.println( MyNum[ i ] ) ;
+        }//end for
+    }//end method
+}//end class
